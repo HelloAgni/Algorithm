fa9f6daa0a3ef426e782d82c770a6ef44624bbaf
Байбак Кирилл <zloifox@gmail.com>
2022-03-05 15:31:17 +0300

    sum of two lowest/largest integers

--- codewars.py
+++ codewars.py
@@ -1,14 +1,17 @@
-def xo(s):
+from heapq import nlargest, nsmallest
+
+
+def sum_two_smallest_numbers(numbers):
     """
-    XO("ooxx") => true
-    XO("xooxx") => false
-    XO("ooxXm") => true
-    XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-    XO("zzoo") => false
+    For example,
+    when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+    [10, 343445353, 3453445, 3453545353453] should return 3453455.
     """
+    mini = nsmallest(2, numbers)
+    maxi = nlargest(2, numbers)
+    print(sum(mini))
+    print(sum(maxi))
 
-    s = s.lower()
-    return s.count('x') == s.count('o')
 
-
-print(xo.__doc__)
+numbers = [19, 0, 42, 0, 77]
+sum_two_smallest_numbers(numbers)
