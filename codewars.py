895c36d8a3769d1129fcd01f591347e3cc8ea7b7
Байбак Кирилл <zloifox@gmail.com>
2022-03-06 13:42:52 +0300

    convert string to camel case

--- codewars.py
+++ codewars.py
@@ -1,27 +1,33 @@
-def abbrev_name(name):
-    """
-    Write a function to convert a name into initials.
-    Str takes two words with one space in between them.
-    Sam Harris => S.H
-    patrick feeney => P.F
-    """
-    x = name.split()
-    abrev = []
-    for first in x:
-        abrev.append(first[0])
-    s = '.'.join(abrev)
-    print(s.upper())
+from re import sub
 
 
-name = 'sam Harris'
-abbrev_name(name)
+def to_camel_case(text):
+    """
+    Convert string to camel case
+    "the-stealth-warrior" gets converted to "theStealthWarrior"
+    "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+    """
+    if text == '':
+        print(text)
+    elif text[0] == text[0].lower():
+        text = sub(r"(_|-)+", " ", text).title().replace(" ", "")
+        print(f'1ый - {text}')
+        first_lower = ''.join([text[0].lower(), text[1:]])
+        print(f'2ый - {first_lower}')
+    else:
+        text = sub(r"(_|-)+", " ", text).title().replace(" ", "")
+        print(f'3ий - {text}')
 
 
 # v2
 
-def abbrev_name_v2(name):
-    print('.'.join(x[0] for x in name.split()).upper())
+def to_camel_case_v2(text):
+    new_text = sub('[_-](.)', lambda x: x.group(1).upper(), text)
+    print(new_text)
 
 
-name = 'Poll Harris'
-abbrev_name_v2(name)
+# text = 'the-stealth-warrior'
+text = 'The_Stealth_Warrior'
+# text = ''
+to_camel_case(text)
+to_camel_case_v2(text)
