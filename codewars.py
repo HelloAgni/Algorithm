383eb6c5a4d96439f0526cdb4807019aa02e2cbf
Байбак Кирилл <zloifox@gmail.com>
2022-03-09 20:01:50 +0300

    form the minimum

--- codewars.py
+++ codewars.py
@@ -1,13 +1,28 @@
-def angle(n):
-    """
-    Find the total sum of internal angles (in degrees),
-    in an n-sided simple polygon.
-    N will be greater than 2.
-    """
-    print((n-2)*180)
+import re
 
+
+def min_value(digits):
+    """
+    Given a list of digits, return the smallest number,
+    that could be formed from these digits,
+    using the digits only once (ignore duplicates).
+    Example:
+    minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+    """
+    x = sorted(set(digits))
+    print(x)
+    print(type(x))
+    y = int(re.sub(r'\W', '', str(sorted(set(digits)))))
+    print(y)
 
 # v2
 
-n = 3
-angle(n)
+
+def min_value_v2(digits_v2):
+    print(int(''.join(map(str, sorted(set(digits_v2))))))
+
+
+digits_v2 = [1, 9, 3, 1, 7, 4, 6, 6, 7]
+digits = [8, 1, 4, 4]
+min_value(digits)
+min_value_v2(digits_v2)
