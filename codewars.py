9fd5866b2482736fbe43f5ddea6cccfc5925ca4a
Байбак Кирилл <zloifox@gmail.com>
2022-03-10 20:26:20 +0300

    check the exam

--- codewars.py
+++ codewars.py
@@ -1,28 +1,34 @@
-import re
-
-
-def min_value(digits):
+def check_exam(arr1, arr2):
     """
-    Given a list of digits, return the smallest number,
-    that could be formed from these digits,
-    using the digits only once (ignore duplicates).
-    Example:
-    minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+    The first input array is the key to the correct answers.
+    The second one contains a student's submitted answers.
+    Return the score for this array of answers,
+    giving +4 for each correct answer,
+    -1 for each incorrect answer, and +0 for each blank answer.
+    If the score < 0, return 0.
+    checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+    checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
     """
-    x = sorted(set(digits))
-    print(x)
-    print(type(x))
-    y = int(re.sub(r'\W', '', str(sorted(set(digits)))))
-    print(y)
+    total = 0
+    for count, x in enumerate(arr2):
+        if x == arr1[count]:
+            total += 4
+        elif x == "":
+            total += 0
+            print('нет ответа')
+        elif x != arr1[count]:
+            total -= 1
+    if total < 0:
+        return 0
+    print(total)
+
 
 # v2
+def check_exam_v2(arr1, arr2):
+    print(max(0, sum(4 if a == b else -1 for a, b in zip(arr1, arr2) if b)))
 
 
-def min_value_v2(digits_v2):
-    print(int(''.join(map(str, sorted(set(digits_v2))))))
-
-
-digits_v2 = [1, 9, 3, 1, 7, 4, 6, 6, 7]
-digits = [8, 1, 4, 4]
-min_value(digits)
-min_value_v2(digits_v2)
+arr1 = ["a", "a", "c", "b"]
+arr2 = ["a", "a", "b",  ""]
+check_exam(arr1, arr2)
+check_exam_v2(arr1, arr2)
