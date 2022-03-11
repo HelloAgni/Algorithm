4d0a887f021a35728ea9348b6be27f25ab467c5d
Байбак Кирилл <zloifox@gmail.com>
2022-03-11 19:23:49 +0300

    convert an array of strings to array of numbers

--- codewars.py
+++ codewars.py
@@ -1,34 +1,22 @@
-def check_exam(arr1, arr2):
+def to_float_array(arr):
     """
-    The first input array is the key to the correct answers.
-    The second one contains a student's submitted answers.
-    Return the score for this array of answers,
-    giving +4 for each correct answer,
-    -1 for each incorrect answer, and +0 for each blank answer.
-    If the score < 0, return 0.
-    checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
-    checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+    Create the function that takes as a parameter a sequence of numbers
+    represented as strings and outputs a sequence of numbers.
+    Example:["1", "2", "3"] to [1, 2, 3]
+    Note that you can receive floats as well.
     """
-    total = 0
-    for count, x in enumerate(arr2):
-        if x == arr1[count]:
-            total += 4
-        elif x == "":
-            total += 0
-            print('нет ответа')
-        elif x != arr1[count]:
-            total -= 1
-    if total < 0:
-        return 0
-    print(total)
+    new = []
+    for x in arr:
+        new.append(float(x))
+    print(new)
 
 
 # v2
-def check_exam_v2(arr1, arr2):
-    print(max(0, sum(4 if a == b else -1 for a, b in zip(arr1, arr2) if b)))
+
+def to_float_array_v2(arr):
+    print(list(map(float, arr)))
 
 
-arr1 = ["a", "a", "c", "b"]
-arr2 = ["a", "a", "b",  ""]
-check_exam(arr1, arr2)
-check_exam_v2(arr1, arr2)
+arr = ["1", "2", "3"]
+to_float_array(arr)
+to_float_array_v2(arr)
