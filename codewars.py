978a950343d618179a8631cafd231174aeb7562b
Байбак Кирилл <zloifox@gmail.com>
2022-03-12 10:42:03 +0300

    longest vowel chain

--- codewars.py
+++ codewars.py
@@ -1,25 +1,27 @@
-def is_triangle(a, b, c):
+import re
+
+
+def solve(s):
     """
-    Implement a function that accepts 3 integer values a, b, c.
-    The function should return true if a triangle can be built
-    with the sides of given length and false in any other case.
-    (In this case, all triangles must have surface greater than 0).
+    The vowel substrings in the word codewarriors are o,e,a,io.
+    The longest of these has a length of 2.
+    Given a lowercase string that has alphabetic characters only
+    (both vowels and consonants) and no spaces,
+    return the length of the longest vowel substring. Vowels are any of aeiou.
     """
-    if (a + b) > c and (b + c) > a and (a + c) > b:
-        print(True)
-    else:
-        print(False)
+    y = re.split(r'(?=[^oeaiou])', s)
+    print(y)
+    long = max(y, key=len)
+    print(len(long)-1)
+
 
 # v2
+def solve_v2(s):
+    print(
+        max(map(len, ''.join(c if c in 'aeiou' else ' ' for c in s).split()))
+        )
 
 
-def is_triangle_v2(a, b, c):
-    print((a < b + c) and (b < a + c) and (c < a + b))
-
-
-a = 7
-b = 2
-c = 2
-is_triangle(a, b, c)
-is_triangle_v2(a, b, c)
-
+s = 'chrononhotonthuooaos'  # 5
+solve(s)
+solve_v2(s)
