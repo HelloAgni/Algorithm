f5959cf1aaa789bafb5b802ae7233c36293a8d62
Байбак Кирилл <zloifox@gmail.com>
2022-03-25 13:31:33 +0300

    case swapping

--- codewars.py
+++ codewars.py
@@ -1,27 +1,40 @@
-import math
 import simple_decorator
 
 
 @simple_decorator.timer
-def strong_num(number):
+def swap(string_):
     """
-    Strong number is the number that the sum of the factorial
-    of its digits is equal to number itself.
-    For example: 145, since
-    1! + 4! + 5! = 1 + 24 + 120 = 145
-    So, 145 is a Strong number.
+    Given a string, swap the case for each of the letters.
+    e.g. CodEwArs --> cODeWaRS
+    Examples:
+    ""           ->   ""
+    "CodeWars"   ->   "cODEwARS"
+    "abc"        ->   "ABC"
+    "ABC"        ->   "abc"
+    "123235"     ->   "123235"
     """
-    s = 0
-    lst = list(str(number))
-    for x in lst:
-        s += math.factorial(int(x))
-    if number == s:
-        print('strong!')
-    else:
-        print('is not strong!')
+    new_string = list(
+        map(lambda x: x.upper() if x == x.lower() else x.lower(), string_)
+    )
+    print(''.join(new_string))
 
 # v2
 
 
-number = 145
-strong_num(number)
+@simple_decorator.timer
+def swap_v2(string_):
+    print(string_.swapcase())
+
+# v3_decorator
+
+
+@simple_decorator.timer
+@simple_decorator.case_swapping
+def swap_v3(string_):
+    print(string_)
+
+
+string_ = 'HelloWorld'
+swap(string_)
+swap_v2(string_)
+swap_v3(string_)
