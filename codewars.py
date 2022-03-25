0112933312365628284c0611b69370047ffa61b3
Байбак Кирилл <zloifox@gmail.com>
2022-03-25 16:02:55 +0300

    find the unique number

--- codewars.py
+++ codewars.py
@@ -1,40 +1,29 @@
+from collections import Counter
+
 from my_decorators import simple_decorator
 
 
 @simple_decorator.timer
-def swap(string_):
+def find_uniq(arr):
     """
-    Given a string, swap the case for each of the letters.
-    e.g. CodEwArs --> cODeWaRS
-    Examples:
-    ""           ->   ""
-    "CodeWars"   ->   "cODEwARS"
-    "abc"        ->   "ABC"
-    "ABC"        ->   "abc"
-    "123235"     ->   "123235"
+    There is an array with some numbers.
+    All numbers are equal except for one. Try to find it!
+    find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+    find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
     """
-    new_string = list(
-        map(lambda x: x.upper() if x == x.lower() else x.lower(), string_)
-    )
-    print(''.join(new_string))
+    for z, x in dict(Counter(arr)).items():
+        if x == 1:
+            print(z)
 
 # v2
 
 
 @simple_decorator.timer
-def swap_v2(string_):
-    print(string_.swapcase())
-
-# v3_decorator
-
-
-@simple_decorator.timer
-@simple_decorator.case_swapping
-def swap_v3(string_):
-    print(string_)
+def find_uniq_v2(arr):
+    a, b = set(arr)
+    print(a if arr.count(a) == 1 else b)
 
 
-string_ = 'HelloWorld'
-swap(string_)
-swap_v2(string_)
-swap_v3(string_)
+arr = [1, 1, 1, 2, 1, 1]  # 2
+find_uniq(arr)
+find_uniq_v2(arr)
