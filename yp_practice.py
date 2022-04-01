e429e5329a81021fcaef46c911374b166ef86fe6
Байбак Кирилл <zloifox@gmail.com>
2022-04-01 14:21:37 +0300

    simple function

--- yp_practice.py
+++ yp_practice.py
@@ -1,13 +1,7 @@
-# visitors - массив номеров пассажиров.
-# Каждый пассажир проехал столько раз, сколько раз встречается его номер
-visitors = [0, 2, 3, 2, 0, 4, 1, 1, 2]
-entries_by_visitor = [0] * 5
-best_visitor = 0
+# На вход через пробел подаются числа a, x, b, c.
+# В конце ввода находится перенос строки.
+# y = ax**2 + bx + c.
 
-for visitor in visitors:
-    entries_by_visitor[visitor] += 1
-    print(entries_by_visitor)
-    if entries_by_visitor[visitor] > entries_by_visitor[best_visitor]:
-        best_visitor = visitor
-
-print(best_visitor)
+a, x, b, c = map(int, (input().split()))
+y = a * (x ** 2) + b * x + c
+print(y)
