7d6d699126b06113b773a4a9351f686c0b181521
Байбак Кирилл <zloifox@gmail.com>
2022-04-02 19:06:44 +0300

    find longest word

--- yp_practice.py
+++ yp_practice.py
@@ -1,45 +1,14 @@
-# Дана матрица. Нужно написать функцию,
-# которая для элемента возвращает всех его соседей.
-import sys
+def find_longest(string):
+    """
+    В первой строке выведите самое длинное слово.
+    Во второй строке выведите его длину. Если подходящих слов несколько,
+    выведите то, которое встречается раньше."""
+    s = string.split(' ')
+    m = max(s, key=len)
+    print(m, len(m), sep='\n')
 
-n = int(input())  # количество строк
-m = int(input())  # количество столбцов
-output_numbers = []
-for i in range(n):
-    line = sys.stdin.readline().rstrip()
-    output_numbers.append(line.split(' '))
-x = int(input())  # координаты по вертикали (начиная с 0)
-y = int(input())  # координаты по горизонтали (начиная с 0)
-if n == 1 and m == 1:
-    print(' ')
-elif 2 >= n > 1 and m == 1:
-    print(output_numbers[x+1][y])
-elif n == 1 and 2 >= m > 1:
-    print(output_numbers[x][y+1])
-elif n == 1 and m > 2 and output_numbers[x][y] == output_numbers[0][y]:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x][y+1]))))
-# elif output_numbers[x][y] == output_numbers[0][0]:
-#     print(' '.join(sorted((output_numbers[x][y+1], output_numbers[x+1][y]))))
-elif output_numbers[x][y] == output_numbers[0][-1]:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x+1][y]))))
-elif output_numbers[x][y] == output_numbers[-1][0]:
-    print(' '.join(sorted((output_numbers[x][y+1], output_numbers[x-1][y]))))
-elif output_numbers[x][y] == output_numbers[-1][-1]:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x-1][y]))))
-# elif output_numbers[x][y] == output_numbers[0][y]:
-#     print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x][y+1],
-#           output_numbers[x+1][y]))))
-# elif output_numbers[x][y] == output_numbers[0][y] and n == 1:
-    # print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x][y+1]))))
-elif output_numbers[x][y] == output_numbers[-1][y]:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x][y+1],
-          output_numbers[x-1][y]))))
-elif output_numbers[x][y] == output_numbers[x][0]:
-    print(' '.join(sorted((output_numbers[x][y+1], output_numbers[x-1][y],
-          output_numbers[x+1][y]))))
-elif output_numbers[x][y] == output_numbers[x][-1]:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x-1][y],
-          output_numbers[x+1][y]))))
-else:
-    print(' '.join(sorted((output_numbers[x][y-1], output_numbers[x][y+1],
-          output_numbers[x-1][y], output_numbers[x+1][y]))))
+
+if __name__ == '__main__':
+    n = int(input())
+    string = input()
+    find_longest(string)
