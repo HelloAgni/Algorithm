3c63236be37cd66d9a09c6ce1bb4785ffe691049
Байбак Кирилл <zloifox@gmail.com>
2022-04-09 16:20:10 +0300

    convert to camel case fix

--- Codewars/convert_to_camel_case.py
+++ Codewars/convert_to_camel_case.py
@@ -6,7 +6,7 @@ def to_camel_case(text):
     Converts dash/underscore delimited words into camel casing.
     The first word within the output should be capitalized only
     if the original word was capitalized
-    Examples
+    Examples:
     "the-stealth-warrior" gets converted to "theStealthWarrior"
     "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
     """
