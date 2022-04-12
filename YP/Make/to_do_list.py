3fd529871b9bd1bf797d98c56230407eb1244a5e
Байбак Кирилл <zloifox@gmail.com>
2022-04-12 13:20:27 +0300

    make to do list

---
+++ YP/Make/to_do_list.py
@@ -0,0 +1,37 @@
+# class Node:
+#     def __init__(self, value, next_item=None):
+#         self.value = value
+#         self.next_item = next_item
+
+#     def solution(node):
+#         while node:
+#             print(node.value)  # optional end=' -> '
+#             node = node.next_item
+#         # print('None')  # if no elements -> None
+
+
+# def test():
+#     node3 = Node("node3 node3", None)
+#     node2 = Node("node2 node2", node3)
+#     node1 = Node("node1 node1", node2)
+#     node0 = Node("node0", node1)
+#     Node.solution(node0)
+
+
+# test()
+
+# Make
+
+
+def solution(node):
+    while node:
+        print(node.value)
+        node = node.next_item
+
+
+def test():
+    node3 = Node("node3 node3", None)
+    node2 = Node("node2 node2", node3)
+    node1 = Node("node1 node1", node2)
+    node0 = Node("node0", node1)
+    solution(node0)
