b7cd15e116eedaa74e31424ba5529296773f7c33
Байбак Кирилл <zloifox@gmail.com>
2022-04-12 20:00:25 +0300

    make first index

---
+++ YP/Make/first_index.py
@@ -0,0 +1,45 @@
+class Node:
+    """
+    Напишите функцию solution, определяющую индекс первого вхождения
+    передаваемого ей на вход значения в связном списке,
+    если значение присутствует.
+    Функция на вход принимает голову односвязного списка и элемент,
+    который нужно найти.
+    """
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
+
+    def solution(node, elem):
+        count = 0
+        while elem != count:
+            if node is None:
+                return -1
+            if elem == node.value:
+                return count   # Искомый индекс элемента
+            else:
+                node = node.next_item
+                count += 1
+
+
+#  Make
+
+# def solution(node, elem):
+#     count = 0
+#     while elem != count:
+#         if node is None:
+#             return -1
+#         if elem == node.value:
+#             return count
+#         else:
+#             node = node.next_item
+#             count += 1
+
+
+# def test():
+#     node3 = Node("node3", None)
+#     node2 = Node("node2", node3)
+#     node1 = Node("node1", node2)
+#     node0 = Node("node0", node1)
+#     idx = solution(node0, "node2")
+#     # result is idx == 2
