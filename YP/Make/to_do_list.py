2c156ecdf8454635d0cec3b2f323203b5fe35db0
Байбак Кирилл <zloifox@gmail.com>
2022-04-14 12:22:46 +0300

    fix2 make to do list

--- YP/Make/to_do_list.py
+++ YP/Make/to_do_list.py
@@ -1,16 +1,28 @@
 class Node:
     """
     Функция должна напечатать элементы списка по одному в строке.
+    >>>from to_do_list import Node, solution
+    >>>node3 = Node("node3 node3", None)
+    >>>node2 = Node("node2 node2", node3)
+    >>>node1 = Node("node1 node1", node2)
+    >>>node0 = Node("node0", node1)
+    >>>solution(node0)
+    node0
+    node1 node1
+    node2 node2
+    node3 node3
+    None
     """
     def __init__(self, value, next_item=None):
         self.value = value
         self.next_item = next_item
 
-    def solution(node):
-        while node:
-            print(node.value)  # optional end=' -> '
-            node = node.next_item
-        print('None')  # if no elements -> None
+
+def solution(node):
+    while node:
+        print(node.value)  # optional end=' -> '
+        node = node.next_item
+    print('None')  # if no elements -> None
 
 
 # Make
