66d182c61c1f0e9a4f56731c42fbe76aeecd27c7
Байбак Кирилл <zloifox@gmail.com>
2022-04-14 12:19:28 +0300

    fix make first index

--- YP/Make/first_index.py
+++ YP/Make/first_index.py
@@ -5,21 +5,31 @@ class Node:
     если значение присутствует.
     Функция на вход принимает голову односвязного списка и элемент,
     который нужно найти.
+    >>>from first_index import Node, solution
+    >>>node3 = Node("node3", None)
+    >>>node2 = Node("node2", node3)
+    >>>node1 = Node("node1", node2)
+    >>>node0 = Node("node0", node1)
+    >>>solution(node0, "node2")
+    2
+    >>>solution(node2, 'node3')
+    1
     """
     def __init__(self, value, next_item=None):
         self.value = value
         self.next_item = next_item
 
-    def solution(node, elem):
-        count = 0
-        while elem != count:
-            if node is None:
-                return -1
-            if elem == node.value:
-                return count   # Искомый индекс элемента
-            else:
-                node = node.next_item
-                count += 1
+
+def solution(node, elem):
+    count = 0
+    while elem != count:
+        if node is None:
+            return -1
+        if elem == node.value:
+            return count   # Искомый индекс элемента
+        else:
+            node = node.next_item
+            count += 1
 
 
 #  Make
