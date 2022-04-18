5b24707d5e80a153a829d2394b0d6bea50988dcf
Байбак Кирилл <zloifox@gmail.com>
2022-04-18 08:52:44 +0300

    remake nearest zero v3

--- YP/Final_task/nearest_zero_v3.py
+++ YP/Final_task/nearest_zero_v3.py
@@ -3,33 +3,20 @@ def nearest(house_numbers, n):
     Выведите расстояние до ближайшего нуля.
     Числа выводите в одну строку, разделяя их пробелами.
     """
-    new = [0] * n
-    first_zero = house_numbers.index(0)
-    last_zero = n - 1 - house_numbers[::-1].index(0)
-
-    if house_numbers.count(0) == 1 and first_zero == 0:
-        for x in range(n):
-            if house_numbers[x] != 0:
-                new[x] = new[x-1] + 1
-
-    if house_numbers.count(0) == 1 and house_numbers[-1] == 0:
-        for x in range(n-1, -1, -1):
-            if house_numbers[x] != 0:
-                new[x] = new[x+1] + 1
-
-    else:
-        for x in range(first_zero, n):
-            if house_numbers[x] != 0:
-                new[x] = new[x-1] + 1
-
-        for x in range(last_zero, first_zero, -1):
-            if house_numbers[x] != 0:
-                new[x] = min(new[x], new[x+1] + 1)
-
-        for x in range(first_zero, -1, -1):
-            if house_numbers[x] != 0:
-                new[x] = new[x+1] + 1
-    return new
+    count = n
+    for i in range(n):
+        if house_numbers[i] == 0:
+            count = 0
+        else:
+            count += 1
+            house_numbers[i] = count
+    for i in range(n - 1, -1, -1):
+        if house_numbers[i] == 0:
+            count = 0
+        else:
+            count += 1
+            house_numbers[i] = min(count, house_numbers[i])
+    return house_numbers
 
 
 if __name__ == '__main__':
