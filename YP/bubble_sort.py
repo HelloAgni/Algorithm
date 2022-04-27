fe67767cad4c96ebc60762c2265951cfa7494383
Байбак Кирилл <zloifox@gmail.com>
2022-04-27 10:04:47 +0300

    remake bubble sort

--- YP/bubble_sort.py
+++ YP/bubble_sort.py
@@ -1,26 +1,23 @@
-def bubble_sort(line):
+def bubble_sort(array, n):
     """
     Сортировка пузырьком.
     После каждого прохода по массиву, на котором
     какие-то элементы меняются местами, выводите его промежуточное состояние.
     """
-    zero_swap = False
-    while True:
-        swapped = False
-        for i in range(len(line)-1):
-            if line[i] > line[i+1]:
-                line[i], line[i + 1] = line[i + 1], line[i]
-                swapped = True
-                zero_swap = True
-        if swapped is True:
-            print(*line)
-        else:
-            break
-    if zero_swap is False:
-        print(*line)
+    change = 1
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+                change = 1
+        if change == 1:
+            print(*array)
+            change = 0
 
 
 if __name__ == '__main__':
     n = int(input())  # Длина массива
-    line = [int(x) for x in input().split()]
-    result = bubble_sort(line)
+    array = [int(x) for x in input().split()]
+    bubble_sort(array, n)
