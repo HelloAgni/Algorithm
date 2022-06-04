4a433d0bec94d18d95ff86ba9c06c22f99329fa5
Байбак Кирилл <zloifox@gmail.com>
2022-06-04 16:14:13 +0300

    count the number of Duplicates

---
+++ Codewars/counting_duplicates.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+def duplicate_count(text):
+    """
+    Write a function that will return the count of distinct case-insensitive
+    alphabetic characters and numeric digits
+    that occur more than once in the input string.
+    Example
+    "abcde" -> 0 # no characters repeats more than once
+    "aabbcde" -> 2 # 'a' and 'b'
+    "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+    """
+    return len([x for x in Counter(text.lower()).values() if x > 1])
+
+
+text = 'Indivisibilities'  # 2
+print(duplicate_count(text))
